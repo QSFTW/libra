43913a32e16e3cefc43de8ef5be761f5b3abbe71
Qiangsheng Gao <qgao5190@uni.sydney.edu.au>
2021-02-01 15:42:14 +0800

    differentiate b/ub multi-transfer command

--- testsuite/cli/src/multi_transfer_commands.rs
+++ testsuite/cli/src/multi_transfer_commands.rs
@@ -1,3 +1,5 @@
+use std::thread;
+use std::time::{Duration, Instant, SystemTime};
 use crate::{client_proxy::ClientProxy, commands::*};
 
 pub struct MultiTransferCommand {}
@@ -11,9 +13,23 @@ impl Command for MultiTransferCommand {
     }
     fn execute(&self, client: &mut ClientProxy, params: &[&str]) {
         let is_blocking = blocking_cmd(&params[0]);
-        let num_iter: i32 = params[4].parse().unwrap();
-        for i in 1..num_iter{
-            client.transfer_coins(&["transfer", &params[1], &params[2], "1", &params[3]], is_blocking);
+        let num_iter: i32 = params[4].parse().unwrap()+1;
+        if is_blocking {
+            for i in 1..num_iter{
+                println!("Timestamp: {:?}", SystemTime::now().duration_since(SystemTime::UNIX_EPOCH));
+                let start = Instant::now();
+                client.transfer_coins(&["transfer", &params[1], &params[2], "1", &params[3]], is_blocking);
+                let duration = start.elapsed();
+                println!("Time elapsed is: {:?}", duration);
+            }
+        } else{
+            let start = Instant::now();
+            for i in 1..num_iter{
+                client.transfer_coins(&["transfer", &params[1], &params[2], "1", &params[3]], is_blocking);
+                thread::sleep(Duration::from_millis(10))
+            }
+            let duration = start.elapsed();
+            println!("Time elapsed is: {:?}", duration);
         }
         return;
     }
